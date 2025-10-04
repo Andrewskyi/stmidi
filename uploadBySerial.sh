a87a15bff56606290f4b802d866170ef36b22cf5
apaluch <andrzejpaluch@gmail.com>
2025-10-04 12:41:03 +0200

    fix naming issues

--- uploadBySerial.sh
+++ uploadBySerial.sh
@@ -1,1 +1,4 @@
-stm32flash -b 115200 -S 0x08000000 -g 0x08000000 -w /mnt/storage/projects/stm32cube_ws/midi2/Release/midi2.bin /dev/ttyUSB0
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+stm32flash -b 115200 -S 0x08000000 -g 0x08000000 -w ${SCRIPT_DIR}/Release/stmidi.bin /dev/ttyUSB0
